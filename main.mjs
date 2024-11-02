5dd2d9ccd2e7a153b61d99188970b09a68d87b06
Szymonito <Szymon.Nowakowski@cyfroweszkoly.pl>
2024-11-02 12:22:44 +0100

    catch

--- main.mjs
+++ main.mjs
@@ -5,3 +5,32 @@ import fs from 'fs'
         input: process.stdin,
         output: process.stdout
     })
+
+    function userInfo(question){
+        return new Promise (resolve => {
+            resolve (answer)
+        })
+    }
+
+    async function main() {
+        try{
+            const name = await userInfo ("Czesc, jak masz na imie?\n")
+            const surename = await userInfo ("Podaj swoje naziwsko\n")
+            const age = await userInfo ("Ile masz lat\n")
+
+
+            const data = {
+                name,
+                surename,
+                age
+            }
+
+            fs.writeFileSync('data.json')
+            console.log ("Dane zostały zapisane w pliku json")
+
+            const fileData = fs.readFileSync('data.json')
+            const dataUser = JSON.parse(fileData)
+            console.log (`Jestes ${dataUser.name} ${dataUser.surename} oraz masz ${dataUser.age} lat.`)
+        }
+    
+    }
